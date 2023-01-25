5092653969d2beb7ad99c990fcdd2cb0c4b3a44d
TheGreyCord <dmitri.matetski@gmail.com>
2023-01-25 20:31:08 +0200

    GUI update
    GUI is under developing!
    Added buttons.

--- main.py
+++ main.py
@@ -1,17 +1,19 @@
 from src.board import Board
 from src.brain import Enemy
+from src.gui import GUI
 
 board = Board()
 enemy = Enemy()
+gui = GUI()
+
 
 board.print_board()
 while not board.is_game_over():
-
     # Player turn
     move = input("Make your move: ")
     move.split(" ")
     print("""
-    
+
     """)
     print("Player move")
     board.make_move(int(move[0]), int(move[2]))
@@ -25,3 +27,4 @@ while not board.is_game_over():
     board.make_move(*enemy.make_move(board))
 
     board.print_board()
+
--- src/gui.py
+++ src/gui.py
@@ -1,14 +1,21 @@
-from tkinter import Tk, Button
+from tkinter import Tk, Button, PhotoImage
+
+# Color scheme
+BACKGROUND = "#658864"
 
 
 def create_board_buttons(root):
+    empty_field = PhotoImage(file="src/images/empty_field.png")
     board_buttons = []
     for column_id in range(0, 3):
         row = []
         for row_id in range(0, 3):
             row.append(Button(
                 root,
-                text="      .       ",
+                image=empty_field,
+                highlightthickness=0,
+                bd=0,
+                activebackground=BACKGROUND,
             ))
             row[row_id].grid(row=row_id, column=column_id)
         board_buttons.append(row)
@@ -17,7 +24,37 @@ def create_board_buttons(root):
 
 class GUI:
     def __init__(self):
-        print("Init")
         self.root = Tk()
-        self.board_buttons = create_board_buttons(self.root)
+        self.root.title("Tic Tac Toe!")
+        self.root.config(padx=10, pady=10, background=BACKGROUND)
+
+        # Create buttons
+        empty_field = PhotoImage(file="src/images/empty_field.png")
+        self.board_buttons = []
+        for row_id in range(0, 3):
+            for column_id in range(0, 3):
+                self.board_buttons.append(Button(
+                    self.root,
+                    image=empty_field,
+                    highlightthickness=0,
+                    bd=0,
+                    activebackground=BACKGROUND,
+                ))
+                self.board_buttons[len(self.board_buttons) - 1].grid(row=row_id, column=column_id)
+
+        #
+        self.board_buttons[0].config(command=lambda: self.press_move_button((0, 0)))
+        self.board_buttons[1].config(command=lambda: self.press_move_button((0, 1)))
+        self.board_buttons[2].config(command=lambda: self.press_move_button((0, 2)))
+        self.board_buttons[3].config(command=lambda: self.press_move_button((1, 0)))
+        self.board_buttons[4].config(command=lambda: self.press_move_button((1, 1)))
+        self.board_buttons[5].config(command=lambda: self.press_move_button((1, 2)))
+        self.board_buttons[6].config(command=lambda: self.press_move_button((2, 0)))
+        self.board_buttons[7].config(command=lambda: self.press_move_button((2, 1)))
+        self.board_buttons[8].config(command=lambda: self.press_move_button((2, 2)))
+
+        self.root.update()
         self.root.mainloop()
+
+    def press_move_button(self, button_id):
+
